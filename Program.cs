3e1d2a8549dea1a4e90dc334e0b12c93e5aac6a5
Andrea <chela355@hotmail.com>
2021-03-01 15:31:10 -0500

    se realizo el ejercicio

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace determinar_par
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un Numero: ");
+            int numer1=Convert.ToInt32(Console.ReadLine());
+            
+            if (numer1 %2 == 0)
+            {
+              Console.WriteLine("Su numero es Par");  
+            }
+        
+            else
+            {
+                Console.WriteLine("Su numero es Impar");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
